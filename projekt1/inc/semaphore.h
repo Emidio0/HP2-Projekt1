fada4eef6b2156ddccdd6a7262f8b1a8e75d855e
Emidio0 <123549193+Emidio0@users.noreply.github.com>
2023-09-08 12:00:19 +0200

    Add files via upload

---
+++ projekt1/inc/semaphore.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+ * @brief Provides semaphores.
+ ********************************************************************************/
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/********************************************************************************
+ * @brief Semaphore attributes. Valid semaphore ID:s are in the range 0 - 31.
+ * 
+ * @param SEM_ID_MIN
+ *        The lowest permitted semaphore ID.
+ * @param SEM_ID_MAX
+ *        The highest permitted semaphore ID.
+ ********************************************************************************/
+#define SEM_ID_MIN 0
+#define SEM_ID_MAX 31
+
+/********************************************************************************
+ * @brief Reserves semaphore with specified ID. The calling thread is blocked
+ *        until the semaphore is available.
+ * 
+ * @param sem_id
+ *        The ID of semaphore to reserve.
+ * @return 
+ *        True upon reserved semaphore, else false if an invalid semaphore ID
+ *        was specified.
+ ********************************************************************************/
+bool semaphore_reserve(const uint8_t sem_id);
+
+/********************************************************************************
+ * @brief Releases semaphore with specified ID. 
+ * 
+ * @param sem_id
+ *        The ID of semaphore to release.
+ * @return 
+ *        True upon released semaphore, else false if an invalid semaphore ID
+ *        was specified.
+ ********************************************************************************/
+bool semaphore_release(const uint8_t sem_id);
+
+#ifdef __cplusplus
+}
+#endif
